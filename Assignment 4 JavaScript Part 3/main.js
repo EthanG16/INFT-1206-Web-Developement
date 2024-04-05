2a0cfe04a88e24c8640888c076cf9e2fe8e43ae3
EthanG16 <ethan.gonta@dcmail.ca>
2024-04-05 00:47:27 -0400

    Assignment 4 Part 3 - Commit 5
    Part 3 of the assignment is now complete.

--- Assignment 4 JavaScript Part 3/main.js
+++ Assignment 4 JavaScript Part 3/main.js
@@ -63,6 +63,19 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
 
 const balls = [];
@@ -89,6 +102,19 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
