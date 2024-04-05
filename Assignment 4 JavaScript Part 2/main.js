e4a74ae05425f28764bb182ffac02731f83ed3ca
EthanG16 <ethan.gonta@dcmail.ca>
2024-04-05 00:15:20 -0400

    Assignment 4 Part 2 Commit 3

--- Assignment 4 JavaScript Part 2/main.js
+++ Assignment 4 JavaScript Part 2/main.js
@@ -13,14 +13,27 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
+const altText = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
+}
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = i; i <= 5; i++) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', "images/pic${i}.jpg");
+  newImage.setAttribute('alt', "images/pic${i}.jpg");
+  thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
+
+
+
